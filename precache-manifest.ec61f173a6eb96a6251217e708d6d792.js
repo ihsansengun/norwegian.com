6e29509753d3114aa196ef2be07e960ccc750288
Ihsan Sengun <me@ihsansengun.com>
2020-10-11 21:49:56 +0100

    Updates

---
+++ precache-manifest.ec61f173a6eb96a6251217e708d6d792.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abae617c25be4a3d247ff08101e02336",
+    "url": "/norwegian.com/index.html"
+  },
+  {
+    "revision": "1580076c4f58758d6c51",
+    "url": "/norwegian.com/static/css/main.71c769e2.chunk.css"
+  },
+  {
+    "revision": "ac96efb57306d591aea7",
+    "url": "/norwegian.com/static/js/2.dd7da66a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/norwegian.com/static/js/2.dd7da66a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1580076c4f58758d6c51",
+    "url": "/norwegian.com/static/js/main.9c81424a.chunk.js"
+  },
+  {
+    "revision": "542c5de5b920be84c057",
+    "url": "/norwegian.com/static/js/runtime-main.228ccca0.js"
+  },
+  {
+    "revision": "0fb9811761b48e6b020ef9566b5254ea",
+    "url": "/norwegian.com/static/media/ApercuPro-Bold.0fb98117.svg"
+  },
+  {
+    "revision": "4830bd16839f2fa6f5e174f4cc308fd5",
+    "url": "/norwegian.com/static/media/ApercuPro-Bold.4830bd16.ttf"
+  },
+  {
+    "revision": "79e6961f29c33b07cadc521e73e98ec8",
+    "url": "/norwegian.com/static/media/ApercuPro-Bold.79e6961f.eot"
+  },
+  {
+    "revision": "b75e7f46e5f69139094eebe93524c8fd",
+    "url": "/norwegian.com/static/media/ApercuPro-Bold.b75e7f46.woff"
+  },
+  {
+    "revision": "f02e3006b3704de4aced3b59b615fcba",
+    "url": "/norwegian.com/static/media/ApercuPro-Bold.f02e3006.otf"
+  },
+  {
+    "revision": "19703667d74962d412cf082a0b09397c",
+    "url": "/norwegian.com/static/media/ApercuPro.19703667.svg"
+  },
+  {
+    "revision": "51d593dcb0e0cd3f8ba6fa08149dede4",
+    "url": "/norwegian.com/static/media/ApercuPro.51d593dc.ttf"
+  },
+  {
+    "revision": "a0a8f5d9ea028c539117169ed13ef5f0",
+    "url": "/norwegian.com/static/media/ApercuPro.a0a8f5d9.woff"
+  },
+  {
+    "revision": "e89c3c01c0a642f01bf628e2c901e70e",
+    "url": "/norwegian.com/static/media/ApercuPro.e89c3c01.eot"
+  },
+  {
+    "revision": "9b1ad1f41068d96cfc5a2c9ec398a0c1",
+    "url": "/norwegian.com/static/media/background.9b1ad1f4.jpg"
+  },
+  {
+    "revision": "d1e6b9bbca5bf3d057cd533689d8cdb1",
+    "url": "/norwegian.com/static/media/facebook_footer.d1e6b9bb.png"
+  },
+  {
+    "revision": "522116b224982fb74082be0f7642b530",
+    "url": "/norwegian.com/static/media/go_icon.522116b2.png"
+  },
+  {
+    "revision": "eaca2f37b4320c8ea636fe2a2d787ee6",
+    "url": "/norwegian.com/static/media/norwegian_logo.eaca2f37.png"
+  },
+  {
+    "revision": "74d286119ee65b2ce7b215ab58d73960",
+    "url": "/norwegian.com/static/media/twitter_footer.74d28611.png"
+  }
+]);
